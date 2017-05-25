e2bcd285ee56276231493d1b0b4a95d75817080e
Cedric Martens <cedricmartens98@gmail.com>
2017-05-25 17:09:58 -0400

    initial commit

---
+++ desktop/src/com/mygdx/hexeditor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.hexeditor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.hexeditor.Hexeditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Hexeditor(), config);
+	}
+}
